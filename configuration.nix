7e51ad81ad01c7cffc85e889218b67518c4a3081
Keio <keiyo.medias@gmail.com>
2026-06-24 20:38:36 +0800

    Cleaned up configuration

--- configuration.nix
+++ configuration.nix
@@ -85,11 +85,6 @@
  ########## System Definitions ############
 
   networking.hostName = "Ivalice"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
